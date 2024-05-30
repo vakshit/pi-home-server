b9fbe2b198f00fc3978dfa34fe3c4aa493412eb8
vakshit <akshitv18@gmail.com>
2024-05-30 11:58:27 +0530

    fix backup scripts

--- backups/database_backup.sh
+++ backups/database_backup.sh
@@ -3,8 +3,10 @@ docker exec -t immich_postgres pg_dumpall --clean --if-exists --username=postgre
 docker exec -t pvt_immich_postgres pg_dumpall --clean --if-exists --username=postgres | gzip > "/backups/pvt-immich.sql.gz"
 
 # create backup for nextcloud
-docker exec -u www-data nextcloud bash -c "php occ maintenance:mode --on"
+docker exec -u www-data nextcloud bash -c "php occ maintenance:mode --on" > /dev/null
 docker exec -t nextcloud-mariadb mysqldump --single-transaction -h localhost -u nextcloud -psecurepassword nextcloud > /backups/nextcloud-mariadb.bak
-docker exec -u www-data nextcloud bash -c "php occ maintenance:mode --off"
+docker exec -u www-data nextcloud bash -c "php occ maintenance:mode --off" > /dev/null
 
 python upload_db.py /backups/immich.sql.gz /backups/pvt-immich.sql.gz /backups/nextcloud-mariadb.bak
+
+echo echo "Backup complete for $(date '+%d-%m-%Y')"
--- backups/upload_db.py
+++ backups/upload_db.py
@@ -10,25 +10,50 @@ FOLDER_ID = os.environ.get("GOOGLE_DRIVE_FOLDER_ID")
 # Define the credentials file
 CREDENTIALS_FILE = os.environ.get("GOOGLE_DRIVE_CREDENTIALS_FILE_PATH")
 
+def get_service():
+    SCOPES = ['https://www.googleapis.com/auth/drive.file']
+    creds = service_account.Credentials.from_service_account_file(CREDENTIALS_FILE, scopes=SCOPES)
+    return build('drive', 'v3', credentials=creds)
 
-def upload_to_drive(file_path, folder_id):
-    SCOPES = ["https://www.googleapis.com/auth/drive.file"]
-    creds = service_account.Credentials.from_service_account_file(
-        CREDENTIALS_FILE, scopes=SCOPES
-    )
-    service = build("drive", "v3", credentials=creds)
+def search_file(service, file_name, folder_id):
+    query = f"name = '{file_name}' and '{folder_id}' in parents and trashed = false"
+    results = service.files().list(q=query, spaces='drive', fields='files(id, name)').execute()
+    items = results.get('files', [])
+    return items[0] if items else None
 
-    file_metadata = {"name": os.path.basename(file_path), "parents": [folder_id]}
+def upload_to_drive(service, file_path, folder_id):
+    file_name = os.path.basename(file_path)
+    existing_file = search_file(service, file_name, folder_id)
+
+    file_metadata = {
+        'name': file_name,
+        'parents': [folder_id]
+    }
     media = MediaFileUpload(file_path, resumable=True)
-    file = (
-        service.files()
-        .create(body=file_metadata, media_body=media, fields="id")
-        .execute()
-    )
-    print(f"File {file_path} uploaded to Google Drive with file ID: {file.get('id')}")
 
+    if existing_file:
+        # print(f"File {file_name} exists. Updating it.")
+        file_id = existing_file['id']
+        updated_file = service.files().update(
+            fileId=file_id,
+            media_body=media
+        ).execute()
+        # print(f"File {file_path} updated on Google Drive with file ID: {updated_file.get('id')}")
+    else:
+        # print(f"Uploading new file {file_name}.")
+        new_file = service.files().create(
+            body=file_metadata,
+            media_body=media,
+            fields='id'
+        ).execute()
+        # print(f"File {file_path} uploaded to Google Drive with file ID: {new_file.get('id')}")
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    # Initialize the Google Drive service
+    service = get_service()
+
+    # Assume the Bash script produces these backup files
     files_to_upload = sys.argv[1:]
     for file_path in files_to_upload:
-        upload_to_drive(file_path, FOLDER_ID)
+        upload_to_drive(service, file_path, FOLDER_ID)
+
