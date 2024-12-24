43865f4410f582ccf826c6b8154a7cafe3abd61c
vakshit-zomato <akshit.verma@zomato.com>
2024-12-24 21:13:49 +0530

    run multiple immich servers with same databases

---
+++ services/immich/create-multiple-postgresql-databases.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+set -u
+
+function create_user_and_database() {
+	local database=$(echo $1 | tr ',' ' ' | awk  '{print $1}')
+	local owner=$(echo $1 | tr ',' ' ' | awk  '{print $2}')
+	local password=$(echo $1 | tr ',' ' ' | awk  '{print $3}')
+	echo "  Creating user '$owner' and database '$database'"
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+	    CREATE USER $owner WITH PASSWORD '$password' SUPERUSER;
+	    CREATE DATABASE $database OWNER $owner;
+	    GRANT ALL PRIVILEGES ON DATABASE $database TO $owner;
+EOSQL
+}
+
+if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
+	echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
+	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ':' ' '); do
+		create_user_and_database $db
+	done
+	echo "Multiple databases created"
+fi
